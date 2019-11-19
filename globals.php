7a033b96db00eac85ceb590a5921bf307287f93e
Adrian Bowyer <contact@reprapltd.com>
2019-11-19 19:00:27 +0000

    Updating globals.php

---
+++ globals.php
@@ -0,0 +1,8 @@
+<?php
+   global $profile, $line, $debug, $debugString, $fileRoot, $fileExtension, $mySlaves, $iAmOn, $iAmMyOwnSlave, 
+	$boostTime, $times, $temps, $profileText, $unixTime, $delimiter, $timeDelimiter, $thermometer, 
+	$summerTime, $onDelay, $offDelay, $creatingTemperatureFile, $temperatureFileContents, $temperatureFileName,
+        $temperatureInterval, $logFileName, $addingToLogFile, $logFileString, $loggingInterval, $units, $building, $location, 
+        $secondsInADay, $temperatureOffset;
+?>
+
