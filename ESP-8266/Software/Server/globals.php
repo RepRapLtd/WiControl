a0b61a1bbe153b6da07112d5896b59b5938098d2
Adrian Bowyer <adrian@reprappro.com>
2019-11-30 15:29:53 +0000

    error string added to globals.

--- ESP-8266/Software/Server/globals.php
+++ ESP-8266/Software/Server/globals.php
@@ -2,5 +2,6 @@
    global $profile, $line, $debug, $debugString, $fileRoot, $fileExtension, $mySlaves, $iAmOn, $iAmMyOwnSlave, 
 	$boostTime, $times, $temps, $profileText, $unixTime, $delimiter, $timeDelimiter, $thermometer, 
 	$summerTime, $onDelay, $offDelay, $creatingTemperatureFile, $temperatureFileContents, $temperatureFileName,
-        $units, $building, $location, $temperatureOffset;
+        $temperatureInterval, $logFileName, $addingToLogFile, $logFileString, $loggingInterval, $units, $building, $location, 
+        $secondsInADay, $temperatureOffset, $errorString;
 ?>
